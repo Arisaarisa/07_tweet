8dd915df684070995dd685a3019e400358d81967
arisa <arisankochira10@gmail.com>
2020-02-16 18:34:36 +0900

    showページからも☆の変更可能

--- show.php
+++ show.php
@@ -31,9 +31,9 @@
         <?php echo h($tweet['content']); ?><br>
         投稿日時: <?php echo h($tweet['created_at']); ?>
         <?php if ($tweet['good'] === '0') : ?>
-          <a style="text-decoration: none;" href="good.php?id=<?php echo h($tweet['id']); ?>"><?php echo '☆'; ?></a>
-        <?php elseif ($tweet['good'] === '1') : ?>
-          <a style="text-decoration: none;" href="good.php?id=<?php echo h($tweet['id']); ?>"><?php echo '★'; ?></a>
+          <a href="good.php?id=<?php echo h($tweet['id']) . "&good=1"; ?>" class="good-link"><?php echo '☆'; ?></a>
+        <?php else : ?>
+          <a href="good.php?id=<?php echo h($tweet['id']) . "&good=0"; ?>" class="bad-link"><?php echo '★'; ?></a>
         <?php endif; ?>
         <a href="edit.php?id=<?php echo h($tweet['id']); ?>">[編集]</a>
         <a href="delete.php?id=<?php echo h($tweet['id']); ?>">[削除]</a>
